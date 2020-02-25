418f14a31aa19f43ae4e9c962d4da9344db3a8d4
eloi-jamao <53044991+eloi-jamao@users.noreply.github.com>
2020-02-25 11:14:56 +0100

    Update DataLoader.py

--- DataLoader.py
+++ DataLoader.py
@@ -1,13 +1,15 @@
 import os
 import json
+import spacy
 
 root = os.getcwd()
 
+'''Loading keypoints'''
 videos_folder = root + '/data/How2Sign_samples 2/How2Sign_samples/openpose_output/json'
 videos = os.listdir(videos_folder)
 keypoints_files = os.listdir(videos_folder + '/' + videos[0])
 
-text = r"This is a one minute video and i don't know what she is saying so i'm just making it up while watching it, also i think all the gestures are very well executed although I don't speak sign language. Tomorrow there will be some sun in the day and some rain in the night."
+
 
 def read_json(filename):
     '''This function takes the json file from openpose
@@ -27,3 +29,12 @@ for i in range(5):
     frame_keypoints = read_json(videos_folder + '/' + videos[0] +'/' + keypoints_files[i])
     print(f'Frame {i} with {len(frame_keypoints)} keypoints and type {type(frame_keypoints)}')
 print(len(keypoints_files))
+
+'''Loading text'''
+text = r"This is a one minute video and i don't know what she is saying so i'm just making it up while watching it, also i think all the gestures are very well executed although I don't speak sign language. Tomorrow there will be some sun in the day and some rain in the night."
+
+spacy_en = spacy.load('en')
+
+def tokenize_en(text):
+    return [tok.text for tok in spacy_en.tokenizer(text)]
+
