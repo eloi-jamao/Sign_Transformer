26b5978ba851717af3a5738ac0af2e2be3ebd450
Eloi Marimon <eloimarimonplastica@gmail.com>
2020-02-26 12:26:04 +0100

    many improvements

--- DataLoader.py
+++ DataLoader.py
@@ -1,16 +1,29 @@
 import os
 import json
 import spacy
+from spacy.vocab import Vocab
+from spacy.language import Language
+import csv
 
 root = os.getcwd()
 
-'''Loading keypoints'''
-videos_folder = root + '/data/How2Sign_samples 2/How2Sign_samples/openpose_output/json'
-videos = os.listdir(videos_folder)
-keypoints_files = os.listdir(videos_folder + '/' + videos[0])
+'''CSV'''
+csv_folder = root + '/data/PHOENIX-2014-T-release-v3/PHOENIX-2014-T/annotations/manual/'
 
+def get_samples(csv_file):
+    '''This function yields the name of a video and
+    the translation in german as an iterator'''
+    first_line = True
+    with open(csv_file) as file:
+        csv_reader = csv.reader(file, delimiter='|')
+        next(csv_reader)
+        for row in csv_reader:
+            yield row[0], row[-1]
 
+def describe_row(name,translation):
+    print(f'Data from iterator: \n Video name of type {type(name)}: {name}\n Translation of type {type(translation)}: {translation}')
 
+'''JSON'''
 def read_json(filename):
     '''This function takes the json file from openpose
     and returns a concatenated list of all keypoints'''
@@ -25,15 +38,45 @@ def read_json(filename):
             keypoints += points
     return keypoints
 
-for i in range(5):
-    frame_keypoints = read_json(videos_folder + '/' + videos[0] +'/' + keypoints_files[i])
-    print(f'Frame {i} with {len(frame_keypoints)} keypoints and type {type(frame_keypoints)}')
-print(len(keypoints_files))
+'''TEXT'''
 
-'''Loading text'''
-text = r"This is a one minute video and i don't know what she is saying so i'm just making it up while watching it, also i think all the gestures are very well executed although I don't speak sign language. Tomorrow there will be some sun in the day and some rain in the night."
+'''This assumes you have a file named vocaulary.txt in the data folder with all the tokens,
+you can get it from https://github.com/neccam/nslt/blob/master/Data/phoenix2014T.vocab.de#L1 '''
+with open(root + '/data/vocabulary.txt') as f:
+    vocabulary = []
+    for row in f:
+        vocabulary.append(row)
 
-spacy_en = spacy.load('en')
 
-def tokenize_en(text):
-    return [tok.text for tok in spacy_en.tokenizer(text)]
+vocab = Vocab(strings = vocabulary)
+nlp = Language(vocab = vocab)
+print(f'Custom language created with {len(nlp.vocab)} vocabulary size')
+
+def sentence_preprocessing(sentence):
+    tokens = [token.text for token in nlp.tokenizer(sentence)]
+    print(tokens)
+
+if __name__ == '__main__':
+
+    '''Loading CSV'''
+    csv_test = csv_folder + 'PHOENIX-2014-T.test.corpus.csv'
+    data_iter = iter(get_samples(csv_test))
+    for i in range(3):
+        video, translation = next(data_iter)
+        #describe_row(video, translation) #Uncomment to print results
+
+
+
+    '''Loading keypoints'''
+    videos_folder = root + '/data/How2Sign_samples 2/How2Sign_samples/openpose_output/json'
+    videos = os.listdir(videos_folder)
+    keypoints_files = os.listdir(videos_folder + '/' + videos[0])
+
+    for i in range(2):
+        frame_keypoints = read_json(videos_folder + '/' + videos[0] +'/' + keypoints_files[i])
+        #print(f'Frame {i} with {len(frame_keypoints)} keypoints and type {type(frame_keypoints)}') #Uncomment to print results
+
+
+    '''Loading text'''
+    text_file_path = root + '/data/test_de.txt'
+    #sentence_preprocessing(translation) #Uncomment to print results
