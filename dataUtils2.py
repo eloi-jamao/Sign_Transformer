e56b9383763fc9835d11af4937062d6bc6d681e3
Daniel Garcia Tello <daniel.gatel@gmail.com>
2020-03-03 11:35:11 +0100

    Update dataUtils2.py
    Dataset frames openPose

--- dataUtils2.py
+++ dataUtils2.py
@@ -1,29 +1,22 @@
 import os
 import json
 import numpy as np
-
-root = os.getcwd()
+import torch
+from torch.utils import data
 
 '''Loading keypoints'''
-videos_folder = os.path.join(root, "data", "How2Sign_samples", "openpose_output", "json")
-videos = os.listdir(videos_folder)
-video_folder = os.path.join(videos_folder, videos[0])
-keypoints_files = os.listdir(video_folder)
+videos_folder = os.path.join(os.getcwd(), "data", "json")
 
 
 def read_json_2_openpose1dframe(filename):
-    '''This function takes the json file from openpose
-    and returns a concatenated list of all keypoints'''
-    # keys = ['pose_keypoints_2d', 'hand_left_keypoints_2d', 'hand_right_keypoints_2d']
     with open(filename) as json_file:
         data = json.load(json_file)
         people_dict = data['people']
         keypoints_dict = people_dict[0]
-
         keypoints_x = []
         keypoints_y = []
         keypoints_c = []
-        keypoints = keypoints_dict['pose_keypoints_2d'][:8*3]
+        keypoints = keypoints_dict['pose_keypoints_2d'][:8 * 3]
         keypoints += keypoints_dict['hand_left_keypoints_2d']
         keypoints += keypoints_dict['hand_right_keypoints_2d']
         for i, point in enumerate(keypoints):
@@ -36,14 +29,51 @@ def read_json_2_openpose1dframe(filename):
                 keypoints_c.append(point)
     return [keypoints_x, keypoints_y, keypoints_c]
 
-//TODO
 
-keypoints = [ [ None for y in range( 50) ]
-             for x in range( 3 ) ]
+def read_sample(sample_path):
+    json_files = os.listdir(sample_path)
+    sample = []
+    for filename in json_files:
+        frame_path = os.path.join(sample_path, filename)
+        frame = read_json_2_openpose1dframe(frame_path)
+        sample.append(frame)
+    return sample
 
-for file in keypoints_files:
-    json_file = os.path.join(video_folder, file)
-    frame_keypoints = read_json(json_file)
-    keypoints = np.stack((keypoints, frame_keypoints), axis=0)
-    # print(f'Frame {i} with {len(frame_keypoints)} keypoints and type {type(frame_keypoints)}')
-print(len(keypoints_files))
+
+class Dataset(data.Dataset):
+
+    def __init__(self, data_openpose_json_dir, labels_openpose_json_dir):
+
+        self.data_folders = os.listdir(data_openpose_json_dir)
+        self.labels_folders = os.listdir(labels_openpose_json_dir)
+
+        self.data_samples = []
+        self.sample_length = 0
+
+        self.data_dirs = []
+        self.labels_dirs = []
+
+        for folder in self.data_folders:
+            sample_data_path = os.path.join(data_openpose_json_dir, folder)
+            self.data_dirs.append(sample_data_path)
+            sample_labels_path = os.path.join(labels_openpose_json_dir, folder)
+            self.labels_dirs.append(sample_labels_path)
+            frames = os.listdir(sample_data_path)
+            self.sample_length = len(frames) if len(frames) > self.sample_length else self.sample_length
+
+    def __len__(self):
+        return len(self.data_folders)
+
+    def __getitem__(self, index):
+
+        data_sample_path = self.data_dirs[index]
+        label_sample_path = self.labels_dirs[index]
+        x = read_sample(data_sample_path)
+        y = read_sample(label_sample_path)
+
+        return x, y
+
+
+ds = Dataset(videos_folder, videos_folder)
+item = ds.__getitem__(0)
+item = ds.__getitem__(1)
