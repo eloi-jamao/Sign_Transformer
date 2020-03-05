bcb62c5a60afa13950e942f46bb825a3ddcbd87f
kimsuazo <quim.suazo@gmail.com>
2020-03-05 09:13:22 +0100

    Dataset inputs done, todo: labels part

--- DataLoader.py
+++ DataLoader.py
@@ -1,15 +1,15 @@
 import os
 from torch.utils.data import Dataset
-import spacy
-from spacy.vocab import Vocab
-from spacy.language import Language
+#import spacy
+#from spacy.vocab import Vocab
+#from spacy.language import Language
 import csv
 import utils
 
 
 
 class SNLT_Dataset(Dataset):
-    def __init__(self, csv_path, train = False):
+    def __init__(self, train = False):
 
         #Read the CSV annotation file and creates a list with (Keypoint path, traduction)
         self.samples = []
@@ -17,33 +17,38 @@ class SNLT_Dataset(Dataset):
         self.train = train
 
         if self.train:
-        	self.keypoints_dir = os.path.join(self.cwd, "data/PHOENIX-2014-T-release-v3/PHOENIX-2014-T/features/keypoints/train")
+            self.keypoints_dir = "/data/PHOENIX-2014-T-release-v3/PHOENIX-2014-T/features/keypoints/train/"
+            self.csv_path = "/data/PHOENIX-2014-T-release-v3/PHOENIX-2014-T/annotations/manual/train_annotations"
         else:
-        	self.keypoints_dir = os.path.join(self.cwd, "data/PHOENIX-2014-T-release-v3/PHOENIX-2014-T/features/keypoints/test")
+            self.keypoints_dir = "./data/keypoints/test/" #"/data/PHOENIX-2014-T-release-v3/PHOENIX-2014-T/features/keypoints/test/"
+            self.csv_path = "./data/annotations/test_annotations.csv"   #"/data/PHOENIX-2014-T-release-v3/PHOENIX-2014-T/annotations/manual/test_annotations.csv"
 
-
-        with open(csv_path) as file:
-        csv_reader = csv.reader(file, delimiter='|')
-        next(csv_reader)
-        for row in csv_reader:
-            self.samples.append((row[0], row[-1]))
+        with open(self.csv_path) as file:
+            csv_reader = csv.reader(file, delimiter='|')
+            next(csv_reader)
+            for row in csv_reader:
+                self.samples.append((row[0], row[-1]))
             
-
     def __len__(self):
         return len(self.samples)
 
     def __getitem__(self, idx):
 
-        #search the keypoint json and convert to list
-        keypoint = json2keypoint(os.path.join(self.keypoints_dir, self.samples[idx][0]))
+        keypoint_sentence = []
+        #search the keypoints json 
+        keypoint_dir = os.path.join(self.keypoints_dir, self.samples[idx][0])
+        for json_file in os.listdir(keypoint_dir):
+            #convert into a list
+            keypoint = utils.json2keypoints(os.path.join(keypoint_dir, json_file))
 
-        #padding the keypoint
-        kp_padding(keypoint, max_len = 475)
+            #padding the keypoint
+            utils.kp_padding(keypoint, max_len = 475)
+            keypoint_sentence.append(keypoint)
 
         #label one hot
         label = self.samples[idx][1]
 
-        return (keypoint, label)
+        return (keypoint_sentence, label)
 
 def describe_row(name,translation):
     print(f'Data from iterator: \n Video name of type {type(name)}: {name}\n Translation of type {type(translation)}: {translation}')
@@ -71,23 +76,13 @@ def print_process(translation):
 
 
 if __name__ == '__main__':
-
-	
-
-	"""
-    root = os.getcwd()
-
-    '''CSV'''
-    csv_folder = root + '/data/PHOENIX-2014-T-release-v3/PHOENIX-2014-T/annotations/manual/'
-
-    '''Loading CSV'''
-    csv_test = csv_folder + 'PHOENIX-2014-T.test.corpus.csv'
-    data_iter = iter(get_samples(csv_test))
-    for i in range(3):
-        video, translation = next(data_iter)
-        describe_row(video, translation) #Uncomment to print results
-
     
+
+
+    dataset = SNLT_Dataset(train = False)
+    for i in range(1):
+        print(len(dataset[i][0]), len(dataset[i][0][0]), dataset[i][1] )
+    """
     '''This assumes you have a file named vocaulary.txt in the data folder with all the tokens,
     you can get it from https://github.com/neccam/nslt/blob/master/Data/phoenix2014T.vocab.de#L1 '''
     with open(root + '/data/vocabulary.txt') as f:
