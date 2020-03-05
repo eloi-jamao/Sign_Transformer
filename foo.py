4fb56e6b3ba9fe2f73b77f55c9acb8d837705272
Eloi Marimon <eloimarimonplastica@gmail.com>
2020-03-05 16:54:50 +0100

    saving changes

--- foo.py
+++ foo.py
@@ -2,19 +2,12 @@ import model
 import utils
 import os
 import torch
-root = os.getcwd()
-videos_folder = root + '/data/How2Sign_samples 2/How2Sign_samples/openpose_output/json'
-videos = os.listdir(videos_folder)
-keypoints_files = os.listdir(videos_folder + '/' + videos[0])
-frames_seq = []
-for i in range(3):
-    frame_keypoints = utils.json2keypoints(videos_folder + '/' + videos[0] +'/' + keypoints_files[i])
-    frames_seq.append(frame_keypoints)
+from torch.utils.data import DataLoader
+import torch.nn as nn
+import DataLoader.py as dl
 
-frames_seq = torch.tensor(frames_seq, dtype = torch.float)
-'adding batch size'
-frames_seq = frames_seq.unsqueeze(dim=1)
-print(frames_seq.shape)
+dataset = dl.SNLT_Dataset(train = False)
+dataloader = DataLoader(dataset, batch_size = 1, shuffle = True)
 
 num_keypoints = 274
 emb_size = 200
@@ -28,13 +21,38 @@ model = model.TransformerModel(
                                nlayers = 2,
                                dropout = 0.2
                                )
-output = model(frames_seq)
-print(output.shape)
-output = output.squeeze(dim=1)
-print(output[0])
-'''
-For now the model is designed for language modeling,
-so we still have some things to change to get a reasonable output.
-But for now, i have changed the first layer of the encoder to be a
-nn.Linear instead of nn.embedding because our input won't be words
-'''
+
+criterion = nn.CrossEntropyLoss()
+
+def train():
+    # Turn on training mode which enables dropout.
+    model.train()
+    total_loss = 0.
+    start_time = time.time()
+    ntokens = len(corpus.dictionary)
+
+    for batch, i in enumerate(range(0, train_data.size(0) - 1, args.bptt)):
+        data, targets = get_batch(train_data, i)
+        # Starting each batch, we detach the hidden state from how it was previously produced.
+        # If we didn't, the model would try backpropagating all the way to start of the dataset.
+        model.zero_grad()
+        output = model(data)
+        loss = criterion(output.view(-1, ntokens), targets)
+        loss.backward()
+
+        # `clip_grad_norm` helps prevent the exploding gradient problem in RNNs / LSTMs.
+        torch.nn.utils.clip_grad_norm_(model.parameters(), args.clip)
+        for p in model.parameters():
+            p.data.add_(-lr, p.grad.data)
+
+        total_loss += loss.item()
+
+        if batch % args.log_interval == 0 and batch > 0:
+            cur_loss = total_loss / args.log_interval
+            elapsed = time.time() - start_time
+            print('| epoch {:3d} | {:5d}/{:5d} batches | lr {:02.2f} | ms/batch {:5.2f} | '
+                    'loss {:5.2f} | ppl {:8.2f}'.format(
+                epoch, batch, len(train_data) // args.bptt, lr,
+                elapsed * 1000 / args.log_interval, cur_loss, math.exp(cur_loss)))
+            total_loss = 0
+            start_time = time.time()
