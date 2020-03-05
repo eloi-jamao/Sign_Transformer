47a854422c6a2763a4eea788312d9e509969783f
Eloi Marimon <eloimarimonplastica@gmail.com>
2020-03-05 13:22:46 +0100

    adding sentence preprocess

--- DataLoader.py
+++ DataLoader.py
@@ -1,8 +1,6 @@
 import os
 from torch.utils.data import Dataset
-#import spacy
-#from spacy.vocab import Vocab
-#from spacy.language import Language
+import spacy
 import csv
 import utils
 
@@ -11,16 +9,17 @@ import utils
 class SNLT_Dataset(Dataset):
     def __init__(self, train = False):
 
-        #Read the CSV annotation file and creates a list with (Keypoint path, traduction)
+        #Read the CSV annotation file and creates a list with (Keypoint path, translation)
         self.samples = []
         self.cwd = os.getcwd()
         self.train = train
+        self.dictionary = Dictionary()
 
         if self.train:
-            self.keypoints_dir = "/data/PHOENIX-2014-T-release-v3/PHOENIX-2014-T/features/keypoints/train/"
+            self.kp_dir = "/data/PHOENIX-2014-T-release-v3/PHOENIX-2014-T/features/keypoints/train/"
             self.csv_path = "/data/PHOENIX-2014-T-release-v3/PHOENIX-2014-T/annotations/manual/train_annotations"
         else:
-            self.keypoints_dir = "./data/keypoints/test/" #"/data/PHOENIX-2014-T-release-v3/PHOENIX-2014-T/features/keypoints/test/"
+            self.kp_dir = "./data/keypoints/test/" #"/data/PHOENIX-2014-T-release-v3/PHOENIX-2014-T/features/keypoints/test/"
             self.csv_path = "./data/annotations/test_annotations.csv"   #"/data/PHOENIX-2014-T-release-v3/PHOENIX-2014-T/annotations/manual/test_annotations.csv"
 
         with open(self.csv_path) as file:
@@ -28,27 +27,59 @@ class SNLT_Dataset(Dataset):
             next(csv_reader)
             for row in csv_reader:
                 self.samples.append((row[0], row[-1]))
-            
+
+
     def __len__(self):
         return len(self.samples)
 
     def __getitem__(self, idx):
 
-        keypoint_sentence = []
-        #search the keypoints json 
-        keypoint_dir = os.path.join(self.keypoints_dir, self.samples[idx][0])
-        for json_file in os.listdir(keypoint_dir):
+        kp_sentence = self.process_kps(self.samples[idx][0])
+        label = self.process_sentence(self.samples[idx][1])
+
+        return (kp_sentence, label)
+
+    def process_kps(self, kp_folder):
+
+        kp_sentence = []
+        #search the keypoints json
+        for json_file in os.listdir(os.path.join(self.kp_dir, kp_folder)):
             #convert into a list
-            keypoint = utils.json2keypoints(os.path.join(keypoint_dir, json_file))
+            kp = utils.json2keypoints(os.path.join(self.kp_dir, kp_folder, json_file))
 
-            #padding the keypoint
-            utils.kp_padding(keypoint, max_len = 475)
-            keypoint_sentence.append(keypoint)
+            #padding the sentence
 
-        #label one hot
-        label = self.samples[idx][1]
+            kp_sentence.append(kp)
 
-        return (keypoint_sentence, label)
+        return kp_sentence
+
+    def process_sentence(self, sentence):
+        tok_sent = [self.dictionary.word2idx[word] for word in sentence.split()]
+        #now introduce the start and end tokens
+        start, end, unk, pad = [self.dictionary.idx2word[i] for i in range(4)]
+        tok_sent.insert(0,self.dictionary.word2idx[start])
+        tok_sent.append(self.dictionary.word2idx[end])
+        #also might need to pad or add unknown tokens where necessary
+        return tok_sent
+
+class Dictionary(object):
+    def __init__(self, vocab_path='./data/vocabulary.txt'):
+
+        self.idx2word = self.read_vocab(vocab_path)
+        self.word2idx = {word:i for i,word in enumerate(self.idx2word)}
+
+    def read_vocab(self, path):
+        vocabulary = []
+        with open(path) as f:
+            for i,line in enumerate(f):
+                word = line.rstrip()
+                vocabulary.append(word)
+
+        return vocabulary
+
+    def __len__(self):
+        return len(self.idx2word)
+
 
 def describe_row(name,translation):
     print(f'Data from iterator: \n Video name of type {type(name)}: {name}\n Translation of type {type(translation)}: {translation}')
@@ -76,29 +107,8 @@ def print_process(translation):
 
 
 if __name__ == '__main__':
-    
-
 
     dataset = SNLT_Dataset(train = False)
+
     for i in range(1):
         print(len(dataset[i][0]), len(dataset[i][0][0]), dataset[i][1] )
-    """
-    '''This assumes you have a file named vocaulary.txt in the data folder with all the tokens,
-    you can get it from https://github.com/neccam/nslt/blob/master/Data/phoenix2014T.vocab.de#L1 '''
-    with open(root + '/data/vocabulary.txt') as f:
-        vocab2int = {}
-        int2vocab = {}
-        for i,row in enumerate(f):
-            row = row.rstrip()
-            vocab2int[row] = i
-            int2vocab[i] = row
-
-
-    vocab = Vocab(strings = int2vocab.values())
-    nlp = Language(vocab = vocab)
-
-    #print(f'Custom language created with {len(nlp.vocab)} vocabulary size')
-    '''Loading text'''
-    text_file_path = root + '/data/test_de.txt'
-    print_process(translation) #Uncomment to print results
-    """
