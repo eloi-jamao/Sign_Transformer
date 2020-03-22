fa9d39500c7d431f4553061036e93e17bb399ee8
kimsuazo <quim.suazo@gmail.com>
2020-03-22 13:48:43 +0100

    New dataloader with gloss almost done

--- DataLoader.py
+++ DataLoader.py
@@ -1,33 +1,36 @@
 import os
 from torch.utils.data import Dataset, DataLoader
-import spacy
 import csv
 import utils
 import torch
 from PIL import Image
 
 class SNLT_Dataset(Dataset):
-    def __init__(self, split, frames_path, csv_path, gloss = False, padding = 475, kp_path = None):
+    def __init__(self, split, frames_path = "data/frames/", csv_path = "data/annotations/", gloss = False, create_vocabulary = False):
 
         splits = ['train','test','dev']
         self.split = split
         if self.split not in splits:
             raise Exception('Split must be one of:', splits)
-        #Read the CSV annotation file and creates a list with (Keypoint path, translation)
-        self.samples = []
-        self.cwd = os.getcwd()
-        self.padding = padding
-        self.dictionary = Dictionary()
-        self.img_dir = os.path.join(frames_path, split)
-        #self.kp_dir = os.path.join(kp_path, split)
-        self.csv_file = csv_path + '/PHOENIX-2014-T.' + self.split + '.corpus.csv'
 
+        self.samples = []
+        self.gloss = gloss
+        #Paths
+        self.img_dir = frames_path + self.split
+        self.csv_path = csv_path
+        self.csv_file = csv_path + self.split + ".csv"
+
+        if self.gloss:
+            self.gloss_dictionary = Dictionary(vocab_path='data/gloss_vocabulary.txt', gloss = True)
+        self.dictionary = Dictionary()
+
+        #Open the csv file and extract img_folder, gloss sentence and label sentence
         with open(self.csv_file) as file:
             csv_reader = csv.reader(file, delimiter='|')
             next(csv_reader)
-            col = -2 if gloss else -1
             for row in csv_reader:
-                self.samples.append((row[0], row[col]))
+                self.samples.append((row[0], row[-2], row[-1]))
+
 
 
     def __len__(self):
@@ -36,49 +39,42 @@ class SNLT_Dataset(Dataset):
     def __getitem__(self, idx):
 
         img_fold = os.path.join(self.img_dir, self.samples[idx][0])
-        label = self.process_sentence(self.samples[idx][1])
 
-        return (img_fold, label)
+        label = self.process_sentence(self.samples[idx][2], self.dictionary)
 
+        if self.gloss:
+            gloss_sent = self.process_sentence(self.samples[idx][1], self.gloss_dictionary)
+            return (img_fold, gloss_sent, label)
+        else:
+            return (img_fold, label)
 
-    def process_sentence(self, sentence):
-
-        start, end, unk, pad = [self.dictionary.idx2word[i] for i in range(4)]
+    def process_sentence(self, sentence, dictionary_ ):
+        #first four words are:
+        start, end, unk, pad = [dictionary_.idx2word[i] for i in range(4)]
         tok_sent = []
+
+        #tokenization using the dictionary
         for word in sentence.split():
-            if word in self.dictionary.idx2word:
-                tok_sent.append(self.dictionary.word2idx[word])
+            if word in dictionary_.idx2word:
+                tok_sent.append(dictionary_.word2idx[word])
             else:
-                tok_sent.append(self.dictionary.word2idx[unk])
-        #print('sentence', sentence)
-        #print('token sentence', tok_sent)
+                tok_sent.append(dictionary_.word2idx[unk])
+        
         #now introduce the start and end tokens
-        tok_sent.insert(0,self.dictionary.word2idx[start])
+        tok_sent.insert(0, dictionary_.word2idx[start])
         tok_sent.append(1) # 1 is the end token
+
         #padding sentence to max_seq
-        for i in range(35 - len(tok_sent)):
-            tok_sent.append(self.dictionary.word2idx[pad])
+        max_seq = 17 if dictionary_.gloss else 27
+        for i in range(max_seq - len(tok_sent)):
+            tok_sent.append(dictionary_.word2idx[pad])
+
         return torch.LongTensor(tok_sent)
 
-    '''
-    def process_kps(self, kp_folder):
-
-        kp_sentence = []
-        #search the keypoints json
-        for json_file in os.listdir(os.path.join(self.kp_dir, kp_folder)):
-            #convert into a list
-            kp = utils.json2keypoints(os.path.join(self.kp_dir, kp_folder, json_file))
-            kp_sentence.append(kp)
-
-        #add padding
-        for i in range(self.padding-len(kp_sentence)):
-        	kp_sentence.append([0 for x in range(len(kp_sentence[0]))])
-
-        return torch.FloatTensor(kp_sentence)
-    '''
-
 class Dictionary(object):
-    def __init__(self, vocab_path='./data/vocabulary.txt'):
+    def __init__(self, vocab_path='data/vocabulary.txt', gloss = False):
+
+        self.gloss = gloss
 
         self.idx2word = self.read_vocab(vocab_path)
         self.word2idx = {word:i for i,word in enumerate(self.idx2word)}
@@ -96,27 +92,22 @@ class Dictionary(object):
         return len(self.idx2word)
 
 
-def describe_row(name,translation):
-    print(f'Data from iterator: \n Video name of type {type(name)}: {name}\n Translation of type {type(translation)}: {translation}')
-
-def decode_sentence(sentence):
-    pass
-
-
 if __name__ == '__main__':
 
-    frames_path = './../Sign_Transformer1/data/PHOENIX-2014-T-release-v3/PHOENIX-2014-T/features/fullFrame-210x260px' #change to your paths
-    csv_path = './../Sign_Transformer1/data/PHOENIX-2014-T-release-v3/PHOENIX-2014-T/annotations/manual'
-
-    dataset = SNLT_Dataset(split = 'test', frames_path = frames_path, csv_path = csv_path, gloss = False)
+    dataset = SNLT_Dataset(split = 'train', gloss = True)
     test_loader = DataLoader(dataset, batch_size = 1, shuffle = False)
 
-
-    for img_path, sentence in test_loader:
-        print('image_directory', img_path)
-        print('sentence', sentence)
+    print(len(dataset))
+    
+    for img_path, gsent, label in dataset:
+        print('image_directory ', img_path)
+        print('gloss_sentence ', len(gsent), gsent)
+        print('label', len(label), label)
         break
-
-
-
-#'./../Sign_Transformer1/data/PHOENIX-2014-T-release-v3/PHOENIX-2014-T/annotations/manual'
+    
+    
+    """
+    - Removed all related with keypoints, saved locally in a Dataloader_old.py script
+    - if gloss flag, output size will be 3, (img_folder, gloss sentence, label sentence), else, output will be (img_folder, label sentence)
+    - todo, create a function flagged to create the vocabulary files when the dataset is instancied 
+    """
