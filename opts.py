35ae6fbe1db2e9c2e9dca76e6f81ef8fbcf10a00
Daniel Garcia Tello <daniel.gatel@gmail.com>
2020-03-28 13:29:10 +0100

    Seq2Seq opts transformer

--- opts.py
+++ opts.py
@@ -4,40 +4,63 @@ import os
 
 def parse_opts():
     parser = argparse.ArgumentParser(description='PyTorch Transformer Language Model')
+
     parser.add_argument('--data', type=str, default=os.path.join(os.getcwd(), 'data'),
                         help='location of the data corpus')
-    parser.add_argument('--emsize', type=int, default=200,
-                        help='size of word embeddings')
-    parser.add_argument('--nhid', type=int, default=200,
-                        help='number of hidden units per layer')
-    parser.add_argument('--nlayers', type=int, default=2,
-                        help='number of layers')
-    parser.add_argument('--lr', type=float, default=20,
-                        help='initial learning rate')
-    parser.add_argument('--clip', type=float, default=0.25,
-                        help='gradient clipping')
-    parser.add_argument('--epochs', type=int, default=40,
-                        help='upper epoch limit')
-    parser.add_argument('--batch_size', type=int, default=20, metavar='N',
-                        help='batch size')
-    parser.add_argument('--bptt', type=int, default=35,
-                        help='sequence length')
-    parser.add_argument('--dropout', type=float, default=0.2,
-                        help='dropout applied to layers (0 = no dropout)')
-    parser.add_argument('--tied', action='store_true',
-                        help='tie the word embedding and softmax weights')
-    parser.add_argument('--seed', type=int, default=1111,
-                        help='random seed')
-    parser.add_argument('--cuda', action='store_true',
-                        help='use CUDA')
-    parser.add_argument('--log-interval', type=int, default=200, metavar='N',
-                        help='report interval')
-    parser.add_argument('--save', type=str, default='model.pt',
-                        help='path to save the final model')
-    parser.add_argument('--onnx-export', type=str, default='',
-                        help='path to export the final model in onnx format')
-    parser.add_argument('--nhead', type=int, default=2,
-                        help='the number of heads in the encoder/decoder of the transformer model')
+
+    # parser.add_argument('--emsize', type=int, default=200,
+    #                     help='size of word embeddings')
+    # parser.add_argument('--nhid', type=int, default=200,
+    #                     help='number of hidden units per layer')
+    # parser.add_argument('--nlayers', type=int, default=2,
+    #                     help='number of layers')
+    # parser.add_argument('--lr', type=float, default=20,
+    #                     help='initial learning rate')
+    # parser.add_argument('--clip', type=float, default=0.25,
+    #                     help='gradient clipping')
+    # parser.add_argument('--epochs', type=int, default=40,
+    #                     help='upper epoch limit')
+    # parser.add_argument('--batch_size', type=int, default=20, metavar='N',
+    #                     help='batch size')
+    # parser.add_argument('--bptt', type=int, default=35,
+    #                     help='sequence length')
+    # parser.add_argument('--dropout', type=float, default=0.2,
+    #                     help='dropout applied to layers (0 = no dropout)')
+    # parser.add_argument('--tied', action='store_true',
+    #                     help='tie the word embedding and softmax weights')
+    # parser.add_argument('--seed', type=int, default=1111,
+    #                     help='random seed')
+    # parser.add_argument('--cuda', action='store_true',
+    #                     help='use CUDA')
+    # parser.add_argument('--log-interval', type=int, default=200, metavar='N',
+    #                     help='report interval')
+    # parser.add_argument('--save', type=str, default='model.pt',
+    #                     help='path to save the final model')
+    # parser.add_argument('--onnx-export', type=str, default='',
+    #                     help='path to export the final model in onnx format')
+    # parser.add_argument('--nhead', type=int, default=2,
+    #                     help='the number of heads in the encoder/decoder of the transformer model')
+
+    # # Resnet params
+    parser.add_argument('--n_classes', default=400, type=int,
+                        help='Number of classes (activitynet: 200, kinetics: 400, ucf101: 101, hmdb51: 51)')
+    parser.add_argument('--resnet_shortcut', default='B', type=str, help='Shortcut type of resnet (A | B)')
+    parser.add_argument('--sample_size', default=112, type=int, help='Height and width of inputs')
+    parser.add_argument('--sample_duration', default=16, type=int, help='Temporal duration of inputs')
+
+    #
+    # # Transformer params
+    parser.add_argument('-e', '--epochs', type=int, default=500, help='upper epoch limit')
+    parser.add_argument('-b', '--b_size', type=int, help='batch size', default=8)
+    parser.add_argument('-cp', '--checkpoint', type=str, default=None, help='checkpoint to load the model')
+    parser.add_argument('-dm', '--d_model', type=int, help='size of intermediate representations', default=512)
+    parser.add_argument('-n', '--n_blocks', type=int, help='number of blocks for the encoder and decoder', default=6)
+    parser.add_argument('-at', '--att_heads', type=int, help='number of attention heads per block', default=8)
+    parser.add_argument('-lr', '--learning_rate', type=float, help='number of attention heads per block', default=0.0)
+    parser.add_argument('-sb', '--src_vocab', type=int, default=None)
+    parser.add_argument('-tb', '--trg_vocab', type=int, default=None)
+    parser.add_argument('-dff', '--d_ff', type=int, default=2048)
+    parser.add_argument('-do', '--dropout', type=float, default=0.1)
 
     args = parser.parse_args()
 
--- seq2seq.py
+++ seq2seq.py
@@ -1,24 +1,78 @@
 import torch.nn as nn
 import model
+# import transformer
+from transformer import *
 import feature_extraction.resnets_3d.resnet as rn
+
 
 # TODO
 class Seq2Seq(nn.Module):
-    def __init__(self, opt_t, opt_r):
+    def __init__(self, opts):
         super(Seq2Seq, self).__init__()
 
-        resnet = rn.resnet34(num_classes=opt_r.n_classes, shortcut_type=opt_r.resnet_shortcut,
-                             sample_size=opt_r.sample_size,
-                             sample_duration=opt_r.sample_duration)
+        resnet = rn.resnet34(num_classes=opts.n_classes, shortcut_type=opts.resnet_shortcut,
+                             sample_size=opts.sample_size,
+                             sample_duration=opts.sample_duration)
         self.extractor = nn.Sequential(resnet.conv1, resnet.bn1, resnet.relu, resnet.maxpool, resnet.layer1,
                                        resnet.layer2, resnet.layer3, resnet.layer4)
-        self.linear = nn.Sequential(nn.Linear(512, opt_t.ntoken), nn.ReLU(inplace=True))
-        self.transformer = model.TransformerModel(opt_t.ntoken, opt_t.ninp, opt_t.nhead, opt_t.nhid, opt_t.nlayers,
-                                                  opt_t.vocab_size, opt_t.max_seq, opt_t.dropout)
+        self.rl = nn.ReLU(inplace=True)
+        # src_vocab, trg_vocab, N=N_blocks, d_model=d_model, h= att_heads
+        # src_vocab, tgt_vocab, N=6, d_model=512, d_ff=2048, h=8, dropout=0.1
+        c = copy.deepcopy
+        attn = MultiHeadedAttention(opts.att_heads, opts.d_model)
+        ff = PositionwiseFeedForward(opts.d_model, opts.d_ff, opts.dropout)
+        position = PositionalEncoding(opts.d_model, opts.dropout)
+        #
+        self.encoder = Encoder(EncoderLayer(opts.d_model, c(attn), c(ff), opts.dropout), opts.n_blocks)
+        self.decoder = Decoder(DecoderLayer(opts.d_model, c(attn), c(attn), c(ff), opts.dropout), opts.n_blocks)
+        self.src_embed = nn.Sequential(Embeddings(opts.d_model, opts.src_vocab), c(position))#TODO
+        self.tgt_embed = nn.Sequential(Embeddings(opts.d_model, opts.trg_vocab), c(position))
+        self.generator = Generator(opts.d_model, opts.trg_vocab)
+        self.transformer = EncoderDecoder(self.encoder, self.decoder, self.src_embed, self.tgt_embed, self.generator)
+
+        # This was important from their code.
+        # Initialize parameters with Glorot / fan_avg.
+        for p in self.transformer.parameters():
+            if p.dim() > 1:
+                nn.init.xavier_uniform_(p)
 
     def forward(self, x):
-        y = self.resnet(x)
-        y = y.view(y.size(0), -1)
-        y = self.linear(y)
-        y = self.transformer(y)
+        # y = self.resnet(x)
+        # y = y.view(y.size(0), -1)
+        # y = self.rl(y)
+        # # y = self.linear(y)
+        y = self.transformer(x)
         return y
+
+# class TransformerOpts(object):
+#     ntoken = None
+#     ninp = None
+#     nhead = None
+#     nhid = None
+#     nlayers = None
+#     vocab_size = None
+#     max_seq = None
+#     dropout = None
+#
+#     def __init__(self, **kwargs):
+#         self.ntoken = kwargs.get('ntoken', None)
+#         self.ninp = kwargs.get('ninp', None)
+#         self.nhead = kwargs.get('nhead', None)
+#         self.nhid = kwargs.get('nhid', None)
+#         self.nlayers = kwargs.get('nlayers', None)
+#         self.vocab_size = kwargs.get('vocab_size', None)
+#         self.max_seq = kwargs.get('max_seq', None)
+#         self.dropout = kwargs.get('dropout', None)
+#
+#
+# class ResnetOpts(object):
+#     n_classes = None
+#     resnet_shortcut = None
+#     sample_size = None
+#     sample_duration = None
+#
+#     def __init__(self, **kwargs):
+#         self.n_classes = kwargs.get('n_classes', None)
+#         self.resnet_shortcut = kwargs.get('resnet_shortcut', None)
+#         self.sample_size = kwargs.get('sample_size', None)
+#         self.sample_duration = kwargs.get('sample_duration', None)
