6bab319745b107145f29e53bf1340d149a029fa2
Eloi Marimon <eloimarimonplastica@gmail.com>
2020-04-08 15:51:49 +0200

    adding multiprocessing

--- train.py
+++ train.py
@@ -4,6 +4,7 @@ import os
 import DataLoader as DL
 import torch
 from torch.utils.data import DataLoader
+import torch.multiprocessing as mp
 import time
 
 parser = argparse.ArgumentParser(description='PyTorch Transformer Training')
@@ -16,6 +17,7 @@ parser.add_argument('-df', '--d_ff', type=int, help='size of feed forward repres
 parser.add_argument('-n', '--n_blocks', type=int, help='number of blocks for the encoder and decoder', default = 6)
 parser.add_argument('-at', '--att_heads', type=int, help='number of attention heads per block', default = 8)
 parser.add_argument('-lr', '--learning_rate', type=float, help='learning rate', default = 0.0)
+parser.add_argument('-w', '--workers', type=int, help='learning rate', default = 2)
 args = parser.parse_args()
 
 #torch.set_default_tensor_type('torch.cuda.FloatTensor')
@@ -47,57 +49,51 @@ else:
 trg_vocab = len(train_dataset.dictionary.idx2word)
 
 
-batch_size = args.b_size
-epochs = args.epochs
-N_blocks = args.n_blocks
-d_model = args.d_model
-d_ff = args.d_ff
-att_heads = args.att_heads
-lr = args.learning_rate
-
-train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, drop_last=True)
-dev_loader = DataLoader(dev_dataset, batch_size=batch_size, shuffle=True, drop_last=True)
-test_loader = DataLoader(test_dataset, batch_size=1, shuffle=True, drop_last=True)
+train_loader = DataLoader(train_dataset, batch_size=args.b_size, shuffle=True, num_workers = args.workers)
+dev_loader = DataLoader(dev_dataset, batch_size=args.b_size, shuffle=True, num_workers = args.workers)
+test_loader = DataLoader(test_dataset, batch_size=1, shuffle=True)
 
 criterion = tf.LabelSmoothing(size=trg_vocab, padding_idx=0, smoothing=0.0)
-model = tf.make_model(src_vocab, trg_vocab, N=N_blocks, d_model=d_model, d_ff=d_ff, h=att_heads)
+model = tf.make_model(src_vocab, trg_vocab, N=args.n_blocks, d_model=args.d_model, d_ff=args.d_ff, h=args.att_heads)
 
 if args.checkpoint is not None:
     model.load_state_dict(torch.load(args.checkpoint))
     print('Loaded state_dict to the model before starting train')
 
 model.to(device)
-model_opt = tf.NoamOpt(d_model, 1, 2000,
-                       torch.optim.Adam(model.parameters(), lr=lr, betas=(0.9, 0.98), eps=1e-9))
+model_opt = tf.NoamOpt(args.d_model, 1, 2000,
+                       torch.optim.Adam(model.parameters(), lr=args.learning_rate, betas=(0.9, 0.98), eps=1e-9))
+if __name__ == '__main__':
+    mp.set_start_method('spawn')
 
-train_losses = []
-dev_losses = []
-best_loss = None
+    train_losses = []
+    dev_losses = []
+    best_loss = None
 
-try:
-    for epoch in range(epochs):
-        print('Starting epoch', epoch)
+    try:
+        for epoch in range(args.epochs):
+            print('Starting epoch', epoch)
 
-        model.train()
-        train_loss = tf.run_epoch(train_loader, model,
-                                  tf.SimpleLossCompute(model.generator, criterion, model_opt),
-                                  device)
+            model.train()
+            train_loss = tf.run_epoch(train_loader, model,
+                                      tf.SimpleLossCompute(model.generator, criterion, model_opt),
+                                      device)
 
-        model.eval()
-        dev_loss = tf.run_epoch(dev_loader, model,
-                                  tf.SimpleLossCompute(model.generator, criterion, None),
-                                  device)
+            model.eval()
+            dev_loss = tf.run_epoch(dev_loader, model,
+                                      tf.SimpleLossCompute(model.generator, criterion, None),
+                                      device)
 
-        train_losses.append(train_loss)
-        dev_losses.append(dev_loss)
+            train_losses.append(train_loss)
+            dev_losses.append(dev_loss)
 
-        if not best_loss or (dev_loss < best_loss):
-            torch.save(model.state_dict(), model_cp)
+            if not best_loss or (dev_loss < best_loss):
+                torch.save(model.state_dict(), model_cp)
 
-        torch.save(train_losses, 'models/G2T/train_losses')
-        torch.save(dev_losses, 'models/G2T/dev_losses')
+            torch.save(train_losses, 'models/G2T/train_losses')
+            torch.save(dev_losses, 'models/G2T/dev_losses')
 
 
-except KeyboardInterrupt:
-    print('-' * 89)
-    print('Exiting from training early')
+    except KeyboardInterrupt:
+        print('-' * 89)
+        print('Exiting from training early')
